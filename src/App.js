24f0ead92a86d50dd6790f1bf62d36dc6d2876a0
Yashwardhan jaiswal <jaiswalyashwardhan20@gmail.com>
2023-03-12 14:06:20 +0530

    added component and atom folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import { Favorite } from '@mui/icons-material';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={}/>
-        <Route path='/' element={}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Favorite/>}/>
         </Routes>
       </BrowserRouter>
     </div>
