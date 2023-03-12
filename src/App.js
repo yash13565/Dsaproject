2f997bcfafbe3ff2500a140fe9278bb1e394dda7
Yashwardhan jaiswal <jaiswalyashwardhan20@gmail.com>
2023-03-12 14:03:02 +0530

    added packages and created routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={}/>
+        <Route path='/' element={}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
